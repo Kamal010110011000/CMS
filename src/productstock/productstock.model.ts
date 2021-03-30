6001099a589b5b9754f3a90e454f962fc816f0ea
Kamal010110011000 <kamalsingh.4iv1@gmail.com>
2021-03-30 17:49:59 +0530

    made endpoints for sms service, profit or loss, gst report, company wise margin

--- src/productstock/productstock.model.ts
+++ src/productstock/productstock.model.ts
@@ -1,4 +1,4 @@
-import *as mongoose from 'mongoose';
+import * as mongoose from 'mongoose';
 import { ApiModelProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer/decorators';
 
